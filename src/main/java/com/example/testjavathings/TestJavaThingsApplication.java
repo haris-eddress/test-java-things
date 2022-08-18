21c81cb3d664f6d0e3c2054c1b4b53a154eea119
haris-eddress <haris@eddress.co>
2022-08-18 19:20:43 +0500

    First commit

---
+++ src/main/java/com/example/testjavathings/TestJavaThingsApplication.java
@@ -0,0 +1,25 @@
+package com.example.testjavathings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+//@SpringBootApplication
+public class TestJavaThingsApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(TestJavaThingsApplication.class, args);
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("h:mm a z").withLocale(Locale.ENGLISH);
+//        LocalDateTime startTime = LocalTime.parse("9:00 am", dateTimeFormatter).atDate(LocalDate.now(ZoneId.of("Canada/Atlantic")));
+        LocalDateTime startTime = LocalTime.parse("9:00 AM", dateTimeFormatter).atDate(LocalDate.now(ZoneId.of("Canada/Atlantic")));
+        System.out.println(startTime);
+    }
+
+}
---
+++ src/test/java/com/example/testjavathings/TestJavaThingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testjavathings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJavaThingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
