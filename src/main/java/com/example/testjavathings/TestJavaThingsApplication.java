4bf254f4d45e55b6044bfd42300690e007cc83ad
haris-eddress <haris@eddress.co>
2022-09-02 12:21:25 +0500

    com 1

--- src/main/java/com/example/testjavathings/TestJavaThingsApplication.java
+++ src/main/java/com/example/testjavathings/TestJavaThingsApplication.java
@@ -1,7 +1,12 @@
 package com.example.testjavathings;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -10,11 +15,22 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+@RestController
+@RequestMapping
 @SpringBootApplication
 public class TestJavaThingsApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(TestJavaThingsApplication.class, args);
     }
+
+
+    @Autowired @Lazy private TestLazyInit testLazyInit;
+    @GetMapping
+    public void getRequest() {
+        System.out.println("getRequest!");
+    }
+    
 
 }
